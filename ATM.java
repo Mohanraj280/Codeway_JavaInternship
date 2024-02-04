5582b71f280797cad95dd55051ac3bd67e3bb170
Mohanraj S <145360247+Mohanraj280@users.noreply.github.com>
2024-02-04 18:15:05 +0530

    Add files via upload

---
+++ ATM.java
@@ -0,0 +1,30 @@
+package ser;
+public class ATM {
+    private BankAccount userAccount;
+
+    public ATM(BankAccount account) {
+        this.userAccount = account;
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= userAccount.getBalance()) {
+            userAccount.setBalance(userAccount.getBalance() - amount);
+            System.out.println("Withdrawal successful. Remaining balance: " + userAccount.getBalance());
+        } else {
+            System.out.println("Invalid amount or insufficient funds.");
+        }
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            userAccount.setBalance(userAccount.getBalance() + amount);
+            System.out.println("Deposit successful. New balance: " + userAccount.getBalance());
+        } else {
+            System.out.println("Invalid amount.");
+        }
+    }
+
+    public double checkBalance() {
+        return userAccount.getBalance();
+    }
+}
